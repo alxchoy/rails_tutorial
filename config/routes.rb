3125bd3ce72aee35e2fe113395c5a56ccf10db4f
Alexander Choy <alexanderchoy18@gmail.com>
2018-07-15 23:36:34 -0500

    finish chapter 7

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,12 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
 
   # Al declarar las rutas de esta forma podemos acceder a ella mediante named routes: help_path, about_path
+
+  # Creamos las rutas para el CRUD de la tabla users
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
